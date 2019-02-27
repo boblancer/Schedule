0b583ee0b87ee93affedad409df60e0edcc85ef5
sartsawatj <sasithornwu@sasithorns-MacBook-Pro.local>
2019-02-27 21:35:28 +0700

    Initial prototype

---
+++ Schedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by sartsawatj on 2/27/19.
+//  Copyright © 2019 boblancer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
